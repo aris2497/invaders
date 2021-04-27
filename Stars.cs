a865b4e5720b5549f2696345a866acd09252d7c8
Paulina Krawczyk <pk3630v@gre.ac.uk>
2021-04-27 14:04:24 +0100

    initial commit

---
+++ Stars.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace invaders
+{
+    class Stars
+    {
+        private struct Star
+        {
+            public Point point;
+            public Pen pen;
+
+            public Star(Point point, Pen pen)
+            {
+                this.point = point;
+                this.pen = pen;
+            }
+        }
+        public void Draw(Graphics g) { }
+        public void Twinkle(Random random) { }
+
+    }
+
+}
